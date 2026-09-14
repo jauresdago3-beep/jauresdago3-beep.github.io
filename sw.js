20a97fc16aa2d075689852239f14b0c79e664fdf
jauresdago3-beep <jauresdago3@gmail.com>
2026-09-14 10:04:55 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-self.pattern = 1
-"domain": "3nbf4.com",
-"zoneId": 11799074
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+self.domain = "3nbf4.com";
+self.zoneId = 11799074;
+self.lary = "";
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
